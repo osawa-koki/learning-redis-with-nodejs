8214fe62d3f835e26d31929005fffd6393647c89
osawa-koki <github@osawa.dev>
2023-07-18 12:00:47 +0900

    Node -> Redis動作確認。

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # learning-redis-with-nodejs
 
 🤡🤡🤡 Node.jsでRedisを操作してみる！  
+
+## 開発環境の構築
+
+```shell
+docker-compose build
+docker-compose run -d --rm --name learning-redis-with-nodejs-redis redis
+docker-compose run -it --rm --name learning-redis-with-nodejs-app app
+```
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
-console.log('Hello World!')
+import Redis from 'ioredis'
+
+(async () => {
+  // Redisクライアントの作成。
+  const redis = new Redis({
+    host: 'redis',
+    port: 6379
+  })
+
+  // イベントハンドラの設定。
+  redis.on('error', (err) => {
+    console.error('Redis error: ', err)
+  })
+  redis.on('connect', () => {
+    console.log('Redis connected!!!')
+  })
+  redis.on('close', () => {
+    console.info('Disconnected...')
+  })
+
+  // Redis操作イロイロ。
+  await redis.set('my-key', 'hogehoge')
+  const value = await redis.get('foo')
+  console.log('my-key: ', value)
+
+  // Redisから切断。
+  await redis.quit()
+})().then(() => {
+  console.log('Done!!!')
+}).catch((err) => {
+  console.error('Error: ', err)
+})
